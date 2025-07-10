6ff69a90585b22324c9c2be619df43413cb8f6cb
Patrick Bradley <bradley.720@osu.edu>
2025-07-10 14:20:56 -0400

    added custom renamer

---
+++ workflow/scripts/fasta_rename_species_custom.R
@@ -0,0 +1,44 @@
+#!/usr/bin/env Rscript
+# Rename species in a FASTA file where the names are "<gene_id>;;<genus>;;<species_id>"
+library(optparse)
+library(seqinr)
+library(purrr)
+library(tibble)
+library(dplyr)
+
+opt_list <- list(
+  make_option(c("-i", "--input"), type="character", help="path to FASTA file to rename"),
+  make_option(c("-m", "--metadata"), type="character", help="path to metadata file"),
+  make_option(c("-g", "--genome_column"), type="character", default="MGnify_accession", help="which column in metadata to match on"),
+  make_option(c("-d", "--id_column"), type="character", default="species_id", help="which column to grab new IDs from"),
+  make_option(c("-r1", "--regex_match"), type="character", default="MGYG-HGUT-", help="regex to apply to genome-column to make it match fasta file"),
+  make_option(c("-r2", "--regex_sub"), type="character", default="MGYG0000", help="replacement text for regex"),
+  make_option(c("-o", "--output"), type="character", help="path to output file")
+)
+prs <- OptionParser(option_list = opt_list)
+p <- parse_args(prs)
+
+# read data and split off species IDs
+fa <- read.fasta(p$input, seqtype="DNA", as.string=TRUE, forceDNAtolower = FALSE)
+md <- read_tsv(p$metadata)
+species_ids <- map_chr(names(fa), ~ str_split_1(string=.x, pattern=";;")[3])
+
+# apply regex 
+md_t <- md
+md_t[, p$genome_column] <- gsub(p$regex_match, p$regex_sub, md_t[, p$genome_column])
+
+# make mapping
+sid_vec <- enframe(species_ids, name="fullname", value=p$genome_column) %>%
+  left_join(md_t) %>%
+  select(fullname, all_of(p$id_column)) %>%
+  deframe()
+
+# map IDs
+new_species_ids <- map_chr(names(fa), ~ {
+  sp <- str_split_1(string=.x, pattern=";;")
+  if (!(.x %in% names(sid_vec))) { warn(paste0("Not found: ", .x))}
+  paste(c(sp[1:2], sid_vec[.x]), collapse=';;')
+})
+
+# output
+write.fasta(fa, names(fa), file.out = p$output)
