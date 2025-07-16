7f89ba77e401347c03b166d962a1b5e066a012b1
Patrick Bradley <bradley.720@osu.edu>
2025-07-16 15:22:16 -0400

    local tweaks?

--- workflow/scripts/fasta_rename_species_custom.R
+++ workflow/scripts/fasta_rename_species_custom.R
@@ -15,7 +15,8 @@ opt_list <- list(
   make_option(c("-d", "--id_column"), type="character", default="species_id", help="which column to grab new IDs from"),
   make_option(c("-r", "--regex_match"), type="character", default="MGYG-HGUT-", help="regex to apply to genome-column to make it match fasta file"),
   make_option(c("-R", "--regex_sub"), type="character", default="MGYG0000", help="replacement text for regex"),
-  make_option(c("-o", "--output"), type="character", help="path to output file")
+  make_option(c("-o", "--output"), type="character", help="path to output file"),
+  make_option(c("-D", "--debug"), type="logical", default=FALSE, help="enable debugging output")
 )
 prs <- OptionParser(option_list = opt_list)
 p <- parse_args(prs)
@@ -24,17 +25,19 @@ p <- parse_args(prs)
 fa <- read.fasta(p$input, seqtype="DNA", as.string=TRUE, forceDNAtolower = FALSE)
 md <- read_tsv(p$metadata)
 species_ids <- map_chr(names(fa), ~ str_split_1(string=.x, pattern=";;")[3])
+names(species_ids) <- names(fa)
 
 # apply regex 
 md_t <- md
-md_t[, p$genome_column] <- gsub(p$regex_match, p$regex_sub, md_t[, p$genome_column])
+md_t[[ p$genome_column]] <- gsub(p$regex_match, p$regex_sub, md_t[[p$genome_column]])
+if (p$debug) print(head(md_t))
 
 # make mapping
 sid_vec <- enframe(species_ids, name="fullname", value=p$genome_column) %>%
   left_join(md_t) %>%
   select(fullname, all_of(p$id_column)) %>%
   deframe()
-print(head(sid_vec))
+if (p$debug) print(head(sid_vec))
 
 # map IDs
 new_species_ids <- map_chr(names(fa), ~ {
