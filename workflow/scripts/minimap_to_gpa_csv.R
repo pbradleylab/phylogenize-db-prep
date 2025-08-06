4e618c9d495ecfac9ea515729526e5affc44c3ca
Patrick Bradley <bradley.720@osu.edu>
2025-08-06 17:34:41 -0400

    added script to pivot minimap wider

---
+++ workflow/scripts/minimap_to_gpa_csv.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+# Based on minimap2 output, create a .csv in gene_presence_absence.csv format (with at least the important columns)
+library(optparse)
+library(readr)
+library(dplyr)
+library(tibble)
+library(tidyr)
+
+opt_list <- list(
+  make_option(c("-i", "--input"), type="character", help="path to tabular minimap2 output file"),
+  make_option(c("-o", "--output"), type="character", help="path to output file")
+)
+prs <- OptionParser(option_list = opt_list)
+p <- parse_args(prs)
+
+mm <- read_tsv(p$input, col_names=FALSE, col_types="cdddccddddddc")
+wide <- group_by(X6) %>%
+  slice_max(X12) %>%
+  separate_wider_delim(X1, delim='_', cols_remove=FALSE, names=c("genome","gene")) %>%
+  select(X6,  genome, X1) %>%
+  pivot_wider(names_from=genome, values_from=X1, values_fn=\(x) paste(x, collapse=';'), values_fill="")
+write_tsv(wide, p$output)
